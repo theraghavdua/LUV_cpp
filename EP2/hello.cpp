c6a8b1fe1aac719f6305d74589d68af46b10a8ef
theraghavdua <raghavdua130@gmail.com>
2025-07-04 11:50:38 +0530

    ep 2

---
+++ EP2/hello.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> //header file, it contains all header files
+using namespace std;//
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << sqrt(2) << endl;
+    cout << "this" << " "  << "\n" << "is" << " " << "a" << " " << "test" << endl;
+    cout << "%d" <<endl;
+    cout<< "ok" <<endl;
+    cout<<sqrt(16) <<endl;
+    //char int float double bool
+    long int lamba;
+    long long int boht_lamba; 
+    int a,b;
+    a = 10;
+    b = 10;
+    int ans = a * 1LL * b; // to convert the answer into long long integer  
+
+    int abc =1;
+    cout<<abc <<endl;
+     abc++; //increment after using
+    cout<<abc <<endl;
+    ++abc; //increment before using
+    cout<<abc <<endl;
+
+    //ASCII values
+    // A = 65
+    // Z = 90
+    // a = 97
+    // b = 122
+    cout<<(int) 'c' ;// typecasting c into intefer to print its ascii value
+    return 0;
+}
